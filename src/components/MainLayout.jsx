047ceabbcb3f6f88c4cdee1f7a69cc5e7888b024
lscantillo <luissebastiancantillo@gmail.com>
2022-11-20 23:05:07 -0500

    Create app and install bootstrap

---
+++ src/components/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+
+import Jumbotron from './Jumbotron'
+
+
+export default function MainLayout() {
+  return (
+    <>
+    <Header/>
+    <Jumbotron/>
+    </>
+  )
+}
